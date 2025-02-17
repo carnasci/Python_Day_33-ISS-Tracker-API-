ec7ed337201970d9448e1ed132e2f9a5b13af6f1
carnasci <cen.master24@gmail.com>
2025-02-17 15:09:25 -0600

    Day 33 - used API, python requests, SMTP, datetime module, and time module to create an application that sends an alert email when the ISS is within 5 degrees of latitude and longitude of location

--- class_work/main.py
+++ class_work/main.py
@@ -1,29 +1,51 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-#
-# data = response.json()
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
-# iss_position = (longitude, latitude)
-# print(iss_position)
+MY_LAT = 38.630280
+MY_LNG = -90.200310
 
-parameters = {
-    "lat":38.630280,
-    "lng":-90.200310,
-    "formatted":0,
-}
+def iss_is_near():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_position = (iss_longitude, iss_latitude)
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
+    if (MY_LAT - 5) < iss_latitude < (MY_LAT + 5) and (MY_LNG - 5) < iss_longitude < (MY_LNG + 5):
+        return True
 
-data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
-print(sunset)
-print(sunrise)
+def send_email():
+    with smtplib.SMTP(host="smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user="carnasci89@gmail.com", password="cxauozxphpreajme")
+        connection.sendmail(
+            to_addrs="carnasci89@gmail.com",
+            from_addr="carnasci89@gmail.com",
+            msg="Subject:Look up!\n\nISS is within view."
+        )
 
-time_now = datetime.now()
-print(time_now.hour)
+def is_night():
+    parameters = {
+        "lat":MY_LAT,
+        "lng":MY_LNG,
+        "formatted":0,
+    }
+
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+while True:
+    time.sleep(60)
+    if iss_is_near() and is_night():
+        send_email()
